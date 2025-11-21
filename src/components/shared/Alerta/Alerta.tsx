4feb2eeab988d9af5421dd65b7b0eb5874709b37
Alan Pisani <alanpisani1993@gmail.com>
2025-11-21 19:00:44 -0300

    aplicado front-modal confirm algo

--- src/components/shared/Alerta/Alerta.tsx
+++ src/components/shared/Alerta/Alerta.tsx
@@ -1,15 +1,23 @@
 import Swal from "sweetalert2";
 
-// interface AlertaProps { 
-//     titulo: string;
-//     texto: string;
-//     icono: "success" | "error" | "warning" | "info" | "question";
-// }
+interface AlertaProps {
+  titulo: string;
+  texto: string;
+  icono: "success" | "error" | "warning" | "info" | "question";
+  isCancelButton?: boolean;
+}
 
-export default function alerta(titulo, texto, icono ) {
-  Swal.fire({
+export default function Alerta({
+  titulo,
+  texto,
+  icono,
+  isCancelButton,
+}: AlertaProps) {
+  return Swal.fire({
     title: titulo,
     text: texto,
     icon: icono,
+    showCancelButton: isCancelButton ? true : false,
+    confirmButtonColor: "#28d3d2"
   });
 }
--- src/pages/Dashboard/ProfessionalPanel/SideMenu/SideMenu.tsx
+++ src/pages/Dashboard/ProfessionalPanel/SideMenu/SideMenu.tsx
@@ -1,29 +1,39 @@
 import { NavLink } from "react-router-dom";
-import "./SideMenu.css"
+import "./SideMenu.css";
 import type { sideMenuDataProps } from "../../../../types/SideMenuProps";
+import Alerta from "../../../../components/shared/Alerta/Alerta";
 
-interface SideMenuProps{
+interface SideMenuProps {
   sideMenuData: sideMenuDataProps[];
 }
 
-export default function SideMenu({ sideMenuData }: SideMenuProps ) {
+export default function SideMenu({ sideMenuData }: SideMenuProps) {
+  const handleSubmit = async () => {
+    const alerta = await Alerta({
+      titulo: "Atencion",
+      texto: "¿Estás seguro que querés cerrar la sesión?",
+      icono: "info",
+      isCancelButton: true,
+    });
 
-  const handleSubmit = () => {
-    if(confirm("¿Estás seguro que querés cerrar la sesión?")){
-      localStorage.removeItem("token");
-      window.location.href = "/conectate";
-    }
-  }
+    if (alerta.isConfirmed) window.location.href = "/conectate";
+  };
 
   return (
     <aside className="side-menu">
       <nav>
-        {
-          sideMenuData.map((item, index) => (
-            <NavLink key={index} to={item.to} className={({ isActive }) => isActive ? "active" : ""}>{ item.name }</NavLink>
-          ))
-        }
-        <button className="cerrar-sesion-profesional" onClick={handleSubmit}>Cerrar sesión</button>
+        {sideMenuData.map((item, index) => (
+          <NavLink
+            key={index}
+            to={item.to}
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            {item.name}
+          </NavLink>
+        ))}
+        <button className="cerrar-sesion-profesional" onClick={handleSubmit}>
+          Cerrar sesión
+        </button>
       </nav>
     </aside>
   );
